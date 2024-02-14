ca6a257d3cf78dc5a4386ddce2b68306a99c0a95
prajodh <prajodhpragaths@gmail.com>
2024-02-13 19:29:03 -0500

    nodemon

--- npm-module.js
+++ npm-module.js
@@ -1,3 +1,11 @@
 const _ = require('lodash')
 const item = [1,[2,[3]]]
 console.log(_.flattenDeep(item))
+
+
+
+
+
+// npm install installs stuff directly from package json
+//npm init -y creates it
+//added node_modules so that we can ignore it
