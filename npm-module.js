99c0ac454c049f62a4c7f999658fd463acec4a71
prajodh <prajodhpragaths@gmail.com>
2024-02-13 19:35:53 -0500

    package changes

--- npm-module.js
+++ npm-module.js
@@ -9,3 +9,7 @@ console.log(_.flattenDeep(item))
 // npm install installs stuff directly from package json
 //npm init -y creates it
 //added node_modules so that we can ignore it
+
+// npm install dependenxcy as dev use -D
+
+// npm run for dev on nodemon
